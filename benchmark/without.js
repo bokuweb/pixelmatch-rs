67b240f7aed25017c4a7738d55399f416e5c7311
bokuweb <bokuweb@bokuwebnoair.lan>
2023-01-28 13:49:05 +0900

    fix

---
+++ benchmark/without.js
@@ -0,0 +1,21 @@
+const PNG = require("pngjs").PNG;
+const { readFileSync } = require("fs");
+
+const without = require("../pixelmatch-wasm/node/cjs/index.cjs");
+
+const data = {
+  base: "../fixtures/001a.png",
+  target: "../fixtures/001b.png",
+  w: 800,
+  h: 578,
+};
+
+(async () => {
+  const { base, target, w, h } = data;
+  const img1 = PNG.sync.read(readFileSync(base)).data;
+  const img2 = PNG.sync.read(readFileSync(target)).data;
+  without.pixelmatch(img1, img2, w, h, {
+    includeAntiAlias: false,
+    threshold: 0.1,
+  });
+})();
